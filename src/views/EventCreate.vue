01b154fc74aea4b4c50d204cfac605941d9dd575
Bobson Lin <bobson801104@gmail.com>
2018-12-10 23:27:42 +0800

    feat: 10. Setters & Getters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,27 @@
 <template>
-  <div><h1>EventCreate</h1></div>
+  <div>
+    <h1>EventCreate</h1>
+    <p>{{ user.id }} {{ user.name }}</p>
+    <p>{{ catLength }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>{{ getEvent(1) }}</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['user', 'categories']),
+    ...mapGetters({
+      catCount: 'catLength',
+      getEvent: 'getEventById'
+    })
+  }
+}
 </script>
 
 <style scoped></style>
